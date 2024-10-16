8867811b778d4094252c5cb7b1b0683bf20336bd
Jerico Lopez <jericolopez2003@gmail.com>
2024-10-16 16:12:53 +0800

    Sample tabs in reports

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -2,8 +2,31 @@
 @include('include.sidenav')
 @section('user-content')
     @vite('resources/js/app.js')
-    <div class="chart-container" style="position: relative; width:10em;">
-        <canvas id="dailyVisitChart"></canvas>
+    <ul class="nav nav-tabs">
+        <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/reports">Daily Visit</a>
+        </li>
+        <li class="nav-item bg-secondary rounded-top">
+            <a class="nav-link text-black" href="/monthly-login">Monthly Visit</a>
+        </li>
+        <li class="nav-item bg-secondary rounded-top">
+            <a class="nav-link text-black" href="/monthly-login">Yearly Visit</a>
+        </li>
+        <li class="nav-item bg-secondary rounded-top">
+            <a class="nav-link text-black" href="/monthly-login">Daily Books Borrowed</a>
+        </li>
+        <li class="nav-item bg-secondary rounded-top">
+            <a class="nav-link text-black" href="/monthly-login">Monthly Books Borrowed</a>
+        </li>
+        <li class="nav-item bg-secondary rounded-top">
+            <a class="nav-link text-black" href="/monthly-login">Yearly Books Borrowed</a>
+        </li>
+    </ul>
+
+    <div class="container">
+        <div class="chart-container" style="position: relative; width:10em;">
+            <canvas id="dailyVisitChart"></canvas>
+        </div>
     </div>
 
         <script>
