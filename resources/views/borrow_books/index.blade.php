cf307f35e03182b45b912bcee05d419d5b35260e
Jerico Lopez <jericolopez2003@gmail.com>
2025-01-06 12:01:29 +0800

    Redesign Borowed book index

--- resources/views/borrow_books/index.blade.php
+++ resources/views/borrow_books/index.blade.php
@@ -10,8 +10,8 @@
         </li>
     </ul>
     <div class="container">
-        <table class="table">
-            <thead class="thead-dark">
+        <table class="table table-bordered">
+            <thead class="table-dark">
                 <tr>
                     <th scope="col">Date</th>
                     <th scope="col">Book</th>
@@ -25,12 +25,20 @@
                 @foreach ($borrowed_books as $borrowed_book)
                     <tr onclick="window.location.href='/borrowed_book/show/{{ $borrowed_book->borrow_id }}';"
                         style="cursor:pointer;">
-                        <td>{{ $borrowed_book->created_at->format('m/d/y h:i a')}}</td>
+                        <td>{{ $borrowed_book->created_at->format('m/d/y h:i a') }}</td>
                         <td>{{ $borrowed_book->title }}</td>
                         <td>{{ $borrowed_book->patron_first_name }} {{ $borrowed_book->patron_last_name }}</td>
                         <td>{{ $borrowed_book->user_first_name }} {{ $borrowed_book->user_last_name }}</td>
                         <td>₱{{ $borrowed_book->fine }}</td>
-                        <td>{{ $borrowed_book->returned ? 'Returned' : 'Not returned' }}
+                        <td>
+                            <span
+                                class="badge
+                                    {{ $borrowed_book->returned ? 'bg-success' : 'bg-warning' }}">
+                                {{ $borrowed_book->returned ? 'Returned' : 'Not Returned' }}
+                            </span>
+                            @if ($borrowed_book->fine)
+                                <span class="badge bg-danger">Overdue</span>
+                            @endif
                         </td>
                     </tr>
                 @endforeach
