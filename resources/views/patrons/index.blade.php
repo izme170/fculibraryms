91563fba1fef7589243b54bd9a0428b84de385f8
Jerico Lopez <jericolopez2003@gmail.com>
2025-01-10 16:32:51 +0800

    Redesign patron index

--- resources/views/patrons/index.blade.php
+++ resources/views/patrons/index.blade.php
@@ -1,28 +1,44 @@
 @extends('layout.main')
 @include('include.sidenav')
 @section('user-content')
-    <div class="container rounded p-3">
-        <a class="btn-simple" href="/patron/create">Add Patron</a>
-        <a class="btn-simple" href="/patrons/export">Export</a>
-        <table class="table">
-            <thead class="thead-dark">
-                <tr>
-                    <th scope="col">Name</th>
-                    <th scope="col">Patron Type</th>
-                    <th scope="col">Contact Number</th>
-                    <th scope="col">Email</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($patrons as $patron)
-                    <tr onclick="window.location.href='/patron/show/{{ $patron->patron_id }}';" style="cursor:pointer;">
-                        <td>{{ $patron->first_name . ' ' . $patron->last_name }}</td>
-                        <td>{{ $patron->type }}</td>
-                        <td>{{ $patron->contact_number }}</td>
-                        <td>{{ $patron->email }}</td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="bg-white rounded p-3" style="min-width: fit-content">
+        <div>
+            <a class="btn-simple" href="/patron/create">Add Patron</a>
+            <a class="btn-simple" href="/patrons/export">Export</a>
+        </div>
+        <div class="d-flex flex-wrap gap-3 mt-3 justify-content-center">
+            @foreach ($patrons as $patron)
+                <a class="card text-decoration-none" href="/patron/show/{{ $patron->patron_id }}"
+                    data-status="{{ $patron->status }}">
+                    <div class="img" style="background-image: url('{{ asset('img/default-patron-image.png') }}');">
+                    </div>
+                    <div class="text">
+                        <p class="h3">{{ $patron->first_name }} {{ $patron->last_name }}</p>
+                        <p class="p">{{ $patron->type }}</p>
+                    </div>
+                </a>
+            @endforeach
+        </div>
     </div>
 @endsection
+
+{{-- <table class="table">
+    <thead class="thead-dark">
+        <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Patron Type</th>
+            <th scope="col">Contact Number</th>
+            <th scope="col">Email</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($patrons as $patron)
+            <tr onclick="window.location.href='/patron/show/{{ $patron->patron_id }}';" style="cursor:pointer;">
+                <td>{{ $patron->first_name . ' ' . $patron->last_name }}</td>
+                <td>{{ $patron->type }}</td>
+                <td>{{ $patron->contact_number }}</td>
+                <td>{{ $patron->email }}</td>
+            </tr>
+        @endforeach
+    </tbody>
+</table> --}}
